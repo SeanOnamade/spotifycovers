eafa06d278d38f00c40892fd45bd328fe9582098
SeanOnamade <100744222+SeanOnamade@users.noreply.github.com>
2025-03-18 11:10:01 -0500

    google site verification

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def index():
             <meta charset="UTF-8">
             <title>Spotify Covers - Create Stunning Album Grids</title>
             <meta name="description" content="Generate beautiful Spotify album cover grids from your playlists or top tracks. Login with Spotify and create your unique collage now!">
+            <meta name="google-site-verification" content="Bb23Njg1oKFgYtGMT3MR_MWG7-MgTFpKymCYPafltpo" />
             <!-- Bootswatch quartz Theme -->
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/quartz/bootstrap.min.css">
             <!-- Google Font -->
@@ -97,6 +98,7 @@ def index():
         <meta charset="UTF-8">
         <title>Spotify Covers - Create Stunning Album Grids</title>
         <meta name="description" content="Generate beautiful Spotify album cover grids from your playlists or top tracks. Login with Spotify and create your unique collage now!">
+        <meta name="google-site-verification" content="Bb23Njg1oKFgYtGMT3MR_MWG7-MgTFpKymCYPafltpo" />
                                   
         <script type="application/ld+json">
         {
